ae7e871a694bc3a062cb3486157ab6072ec4de39
hide card <hidecard1500@gmail.com>
2026-01-28 15:21:18 +0630

    database create and product and customer insert database

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -23,6 +23,56 @@ class DatabaseService {
 
   Future<Database> _initDB() async {
     String path = join(await getDatabasesPath(), dbname);
-    return await openDatabase(path, version: 1);
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future<void> _onCreate(Database db, int version) async {
+    await db.execute('''
+        CREATE TABLE $tableproduct(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        price REAL,
+        imageUrl TEXT,
+        stock INTEGER
+    ''');
+    await db.execute('''
+        CREATE TABLE $tablecustomer(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        email TEXT,
+        phone TEXT
+        )
+''');
+    await db.execute('''
+        CREATE TABLE $tableorder(
+        id INTEGER PRIMARY KEY,
+        customerID INTEGER,
+        items TEXT,
+        total REAL,
+        date TEXT
+        )
+''');
+
+await db.insert(tableproduct, {
+      'id': 1,
+      'name': 'Coffee',
+      'price': 4.99,
+      'imageUrl': '',
+      'stock': 100,
+    });
+    await db.insert(tableproduct, {
+      'id': 2,
+      'name': 'Sandwich',
+      'price': 8.99,
+      'imageUrl': '',
+      'stock': 50,
+    });
+    await db.insert(tablecustomer, {
+      'id': 1,
+      'name': 'John Doe',
+      'phone': '1234567890',
+      'email': 'john@example.com',
+    });
+
   }
 }
